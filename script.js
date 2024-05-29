7d66de838834c72fd7ad13a141e000df0cca46e7
denifisman <104898712+denifisman@users.noreply.github.com>
2024-05-29 16:09:25 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateSurat() {
+    var namaAnak = document.getElementById("namaAnak").value;
+    var namaAyah = document.getElementById("namaAyah").value;
+    var tempatTanggalLahirAyah = document.getElementById("tempatTanggalLahirAyah").value;
+    var jenisKelaminAyah = document.getElementById("jenisKelaminAyah").value;
+    var agamaAyah = document.getElementById("agamaAyah").value;
+    var pekerjaanAyah = document.getElementById("pekerjaanAyah").value;
+
+    var namaIbu = document.getElementById("namaIbu").value;
+    var tempatTanggalLahirIbu = document.getElementById("tempatTanggalLahirIbu").value;
+    var jenisKelaminIbu = document.getElementById("
